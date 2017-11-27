3c3bb9a9b1efc3b968ed17f0eb14ebac1035b77b
Hugo Zink <hugo.zink@hotmail.com>
2017-11-27 21:18:57 +0100

    Removed test data insertion.

--- model/category.model.js
+++ model/category.model.js
@@ -22,28 +22,4 @@ const CategorySchema = new Schema({
 
 const Category = mongoose.model('category', CategorySchema);
 
-// Add a 'dummy' category (every time you require this file!)
-const category = new Category({
-    name: 'Pizza & Pasta',
-    recipes: [{
-        name: 'Pepperoni Pizza',
-        description: 'A delicious pizza.',
-        imagePath: 'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
-        ingredients: [
-            {
-                name: 'Dough',
-                amount: 1
-            },
-            {
-                name: 'Cheese',
-                amount: 5
-            },
-            {
-                name: 'Tomato Sauce',
-                amount: 1
-            }
-        ]
-    }]
-}).save();
-
 module.exports = Category;
